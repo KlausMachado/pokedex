9eb07395a688bc979decc35f07285fbe2cfb7075
Carla Machado <carlamrosa438@gmail.com>
2022-05-12 11:52:39 -0300

    updates if/else

--- src/component/cards/renderPokemonList.js
+++ src/component/cards/renderPokemonList.js
@@ -1,6 +1,6 @@
 import { getPokemons, getColectionPokemons } from "../getPokemon";
 import { useContext, useEffect, useState } from "react";
-import { Button, Section } from "./tags-stayle";
+import { Button, Section, H1 } from "./tags-stayle";
 import { PokemonsList } from "./pokemonList";
 import { numberLimit } from "../variables";
 import { ThemeContext } from "../../contexts/themeContext";
@@ -28,23 +28,29 @@ const ColectionOfPokemons = () => {
 
   const { theme } = useContext(ThemeContext);
 
-  return (
-    <Section style={{ color: theme.color, background: theme.background }}>
-      <PokemonsList
-        pokemons={colection.pokemons}
-        style={{ color: theme.color, background: theme.background }}
-      />
-      <Button
-        style={{ color: theme.color, background: theme.background }}
-        onClick={async () => {
-          numberLimit.value += 10;
-          setPokemon();
-        }}
-      >
-        Mais Pokemons
-      </Button>
-    </Section>
-  );
+  if (colection.pokemons.length !== 0) {
+    return (
+      <Section style={{ color: theme.color, background: theme.background }}>
+        <PokemonsList
+          pokemons={colection.pokemons}
+          style={{ color: theme.color, background: theme.background }}
+        />
+        <Button
+          style={{ color: theme.color, background: theme.background }}
+          onClick={async () => {
+            numberLimit.value += 10;
+            setPokemon();
+          }}
+        >
+          Mais Pokemons
+        </Button>
+      </Section>
+    );
+  } else {
+    <Section>
+      <H1>Carregando...</H1>
+    </Section>;
+  }
 };
 
 export default ColectionOfPokemons;
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { PokemonCardInfo } from "../component/cards/pokemonCard";
+import { PokemonCardInfo } from "../component/cards/renderPokemonDetails";
 import ColectionOfPokemons from "../component/cards/renderPokemonList";
 
 export const AppRoutes = () => {
